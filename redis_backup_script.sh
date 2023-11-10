901fc2307fd31490c66f4dc1a2ec6fc1b68585a4
김무건 <pos04167@kakao.com>
2023-11-11 00:56:38 +0900

    Update redis_backup_script.sh

--- redis_backup_script.sh
+++ redis_backup_script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# 사용자 프로파일 로드
+. ~user1/.bash_profile
+
 # Redis CLI를 사용하여 백그라운드에서 스냅샷 생성
-docker exec -it study-redis redis-cli BGSAVE
+docker exec study-redis redis-cli BGSAVE
 
 # 백그라운드 세이브 작업이 완료될 때까지 대기
 sleep 10
@@ -10,4 +13,4 @@ sleep 10
 backup_filename="dump_$(date +"%Y%m%d%H%M%S").rdb"
 
 # 생성된 스냅샷 파일을 호스트의 안전한 위치로 복사
-docker cp study-redis:/data/dump.rdb "/home/ec2-user/CStudy_BackEnd/redis_backup/$backup_filename"
+docker cp study-redis:/data/dump.rdb "/home/ubuntu/CStudy_Infra/$backup_filename"
